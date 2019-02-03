eaf39d28249cba8e335fa3f2c963950c7f510938
tomxgithub <tomxgithub@users.noreply.github.com>
2019-02-02 18:31:44 -0600

    commit

---
+++ src/main/java/com/example/springbootopenshiftdemo/SpringbootOpenshiftdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenshiftdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenshiftdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeservice/RcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.timeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
